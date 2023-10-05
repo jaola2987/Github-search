eb43a24c2bf44e9bc1fc0819d6d44ec727c3da01
jaola2987 <jaola2987@gmail.com>
2023-10-06 01:43:03 +0300

    Fixed the useDebounce function

--- providers/FetchProvider.tsx
+++ providers/FetchProvider.tsx
@@ -10,28 +10,12 @@ import {
   PropsWithChildren,
   useCallback,
 } from "react";
-import debounce from "lodash.debounce";
 
 import { IFetchContextProps, Repo } from "./fetchProvider.interface";
 import { FetchService } from "@/services/fetch.service";
+import { useDebounce } from "@/hooks/useDebounce";
 
 const FetchContext = createContext({} as IFetchContextProps);
-
-// function useDebounce(value: string, delay: number) {
-//   const [debouncedValue, setDebouncedValue] = useState(value);
-
-//   useEffect(() => {
-//     const handler = setTimeout(() => {
-//       setDebouncedValue(value);
-//     }, delay);
-
-//     return () => {
-//       clearTimeout(handler);
-//     };
-//   }, [value, delay]);
-
-//   return debouncedValue;
-// }
 
 export const FetchlProvider: FC<PropsWithChildren<unknown>> = ({
   children,
@@ -42,37 +26,24 @@ export const FetchlProvider: FC<PropsWithChildren<unknown>> = ({
 
   const handleError = useCallback((e: string) => setError(e), []);
 
-  const debouncedSetUsername = debounce(
-    (username: string) => setUsername(username),
-    300
-  );
+  const handleUserName = useCallback((e: string) => {
+    setUsername(e);
+  }, []);
 
-  const handleUserName = useCallback(
-    (e: string) => debouncedSetUsername(e),
-    [debouncedSetUsername]
-  );
-
-  // const handleUserName = useCallback((e: string) => setUsername(e), []);
-
-  // const debouncedUsername = useDebounce(username, 300);
-
-  // useEffect(() => {
-  //   if (debouncedUsername) {
-  //     debouncedFetchData(debouncedUsername);
-  //   }
-  // }, [debouncedUsername, debouncedFetchData]);
+  const debouncedUsername = useDebounce(username, 500);
 
   useEffect(() => {
-    async function qwe(username: string, handleError: (e: string) => void) {
-      const data = await FetchService.getData(username, handleError);
-      setRepos(data);
-      console.log("dd", data);
+    if (debouncedUsername) {
+      async function fetchData(
+        username: string,
+        handleError: (e: string) => void
+      ) {
+        const data = await FetchService.getData(username, handleError);
+        setRepos(data);
+      }
+      fetchData(debouncedUsername, handleError);
     }
-
-    if (username) {
-      qwe(username, handleError);
-    }
-  }, [handleError, username]);
+  }, [debouncedUsername, handleError]);
 
   const providerValue = useMemo(
     () => ({
